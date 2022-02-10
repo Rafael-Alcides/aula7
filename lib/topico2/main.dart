1a9da5c7190507d8db7db57cc748aa19d9351a1d
Rafael Alcides <rafaelalci10@gmail.com>
2022-02-10 18:55:20 -0300

    Topico 2

--- lib/topico2/main.dart
+++ lib/topico2/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:topicos/topico2/theme.dart';
 
 void main() {
   runApp(const MyAppBar());
@@ -9,9 +10,11 @@ class MyAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home:Appbar()
-    );
+    return MaterialApp(
+        title: "Meu Drawer",
+        debugShowCheckedModeBanner: false,
+        theme: ThemeAppBar.ligth,
+        home: const Appbar());
   }
 }
 
@@ -20,27 +23,47 @@ class Appbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
-      endDrawer:
-        Drawer(
-          backgroundColor: Colors.pink[100],
-          child: ListView(
-            children: const [
-
-            ],
+      endDrawer: Drawer(
+        backgroundColor: Colors.pink[50],
+        child: ListView(
+          children: const [
+            ListTile(
+              leading: Icon(Icons.flash_on,
+              size: 32, color: Colors.blue,),
+              title: Text("Flutter"),
+              subtitle: Text("Tudo são widget"),
+              trailing: Icon(Icons.navigate_next),
+            ),
+            ListTile(
+              leading: Icon(Icons.mood,
+              size: 32, color: Colors.red,),
+              title: Text("Dart"),
+              subtitle: Text("É muito Forte"),
+              trailing: Icon(Icons.navigate_next),
+            ),
+            ListTile(
+              leading: Icon(Icons.local_cafe,
+              size: 32, color: Colors.brown,),
+              title: Text("Cafessineo"),
+              subtitle: Text("Quero cafeé!"),
+              trailing: Icon(Icons.navigate_next),
+            )
+          ],
         ),
       ),
       appBar: AppBar(
-        backgroundColor: Colors.pink, 
-        title: Text("Meu Drawer",
-        style: TextStyle(
-          fontSize: 32.0,
-          color: Colors.pink[100],
+        
+        titleTextStyle: TextStyle(color: Colors.pink[100]),
+        title:const Text(
+          "Meu Drawer",
+          style: TextStyle(
+            fontSize: 32.0,
+          ),
         ),
-        ),
-        iconTheme: IconThemeData(color: Colors.pink[100]),
-      ), 
-      
+        iconTheme: IconThemeData(color: Colors.pink[100], size: 40),
+      ),
     );
   }
 }
---
+++ lib/topico2/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ThemeAppBar {
+  static final ThemeData ligth =
+      ThemeData(brightness: Brightness.light, primarySwatch: Colors.pink);
+}
